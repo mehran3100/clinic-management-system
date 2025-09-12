34c9859bfbc16f06d51ce6dcdd3a8d8533d996e8
Mehran Zare <mehran.zare3100@gmail.com>
2025-09-12 21:02:30 +0330

    fixed the tests
    1) fixed test classes package name mismatch and removed test class for eureka
    2) removed spring boot app classes from common modules (common-core and common-kafka)

--- api-gateway/src/test/java/com/clinic/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/clinic/apigateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,12 @@
-package com.clinic.discovery_server;
+package com.clinic.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class DiscoveryServerApplicationTests {
+@ActiveProfiles("test")
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
